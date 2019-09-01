49c2227906331b6f0cd8e36d20cab4be5a983bce
Noelia Donato <noeliadonato@gmail.com>
2019-08-31 21:26:31 -0300

    150 sin terminar

--- S10_SQL/util/database.js
+++ S10_SQL/util/database.js
@@ -1,4 +1,3 @@
-// const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete','root','$gia.69$', {dialect: 'mysql', host: 'localhost'});
