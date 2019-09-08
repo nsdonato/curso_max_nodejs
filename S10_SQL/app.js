8841ce52e7ba3fb3ca655d9bf4f7d0ee7ddae6e1
Noelia Donato <noeliadonato@gmail.com>
2019-09-08 00:47:28 -0300

    173 finalizado

--- S10_SQL/app.js
+++ S10_SQL/app.js
@@ -7,6 +7,10 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -35,9 +39,16 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product,  { through: OrderItem });
 
 sequelize
-  // .sync({ force: true })
+  //.sync({ force: true })
   .sync()
   .then(result => {
     return User.findByPk(1);
@@ -50,6 +61,10 @@ sequelize
     return user;
   })
   .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     // console.log(user);
     app.listen(3000);
   })
--- S10_SQL/util/database.js
+++ S10_SQL/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'mysqlGia09', {
+const sequelize = new Sequelize('node-complete', 'root', '$gia.69$', {
   dialect: 'mysql',
   host: 'localhost'
 });
